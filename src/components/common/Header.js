d4d0a0fe0d848d827798398d22b17a0b2a44be83
SurrenderAt20 <54646142+SurrenderAt20@users.noreply.github.com>
2023-05-16 21:00:13 +0200

    header and navigation

---
+++ src/components/common/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Navigation } from "./Navigation";
+import "./Header.css";
+
+export const Header = () => {
+  return (
+    <div className="container">
+      <Navigation />
+    </div>
+  );
+};
---
+++ src/components/common/Navigation.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Navigation.css";
+import logored from "../../images/logored.png";
+
+export const Navigation = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        {" "}
+        <img className="logoImg" src={logored} alt="Logo" />
+        FITIN
+      </div>
+      <div className="links">
+        <a href="/">Link 1</a>
+        <a href="/">Link 2</a>
+        <a href="/">Link 3</a>
+      </div>
+    </nav>
+  );
+};
