3000110b45340a9a1b683e7aeac01a9c55571501
SurrenderAt20 <54646142+SurrenderAt20@users.noreply.github.com>
2023-05-18 17:27:19 +0200

    about done

--- src/components/common/About.js
+++ src/components/common/About.js
@@ -1,11 +1,37 @@
 import React from "react";
 import "./About.css";
 import { SixGrid } from "../smallComponents/SixGrid";
+import fitpng from "../../images/fitpng.png";
 
 export const About = () => {
   return (
     <div className="containerAbout">
       <SixGrid />
+      <div className="contentContainer">
+        <div className="contentWrapper">
+          {/*           <div className="contentCube">
+            <div className="contentItem">
+              <img src={fitpng} />
+            </div>
+          </div> */}
+          <div className="contentCube">
+            <div className="contentItem">
+              <h3>membership offer of the year</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                gravida consequat. Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Nunc gravida consequat. Lorem ipsum dolor sit
+                amet, consectetur adipiscing elit. Nunc gravida consequat. Lorem
+                ipsum dolor sit amet, consectetur adipiscing elit. Nunc gravida
+                consequat. Lorem ipsum dolor sit amet, consectetur adipiscing
+                elit. Nunc gravida consequat. Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. Nunc gravida consequat.
+              </p>
+              <div className="btnAbout">Learn More</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
