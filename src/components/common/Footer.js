359c539aea6dc44ad94227a67f01b9b08ab09ba1
SurrenderAt20 <54646142+SurrenderAt20@users.noreply.github.com>
2023-05-26 17:11:56 +0200

    footer skellet

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,10 +1,24 @@
 import React from "react";
 import "./Footer.css";
+import {
+  AiFillFacebook,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiFillTwitterSquare,
+} from "react-icons/ai";
 
 export const Footer = () => {
   return (
     <div className="containerFooter">
-      <div className="wrapperFooter"></div>
+      <div className="wrapperFooter">
+        <div className="itemSectionOne">
+          <AiFillFacebook className="iconFooter" color="#fff" size={30} />
+          <AiFillInstagram className="iconFooter" color="#fff" size={30} />
+          <AiFillLinkedin className="iconFooter" color="#fff" size={30} />
+          <AiFillTwitterSquare className="iconFooter" color="#fff" size={30} />
+        </div>
+        <div className="borderFooter"></div>
+      </div>
     </div>
   );
 };
